9ed4fe209a6e555275b6282ea889a618cfd92917
nireo <eemil.lehtikangas1@gmail.com>
2020-12-14 00:20:13 +0200

    added somewhat proper file encryption

--- main.go
+++ main.go
@@ -6,29 +6,33 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
-	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 func createHash(key string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(key))
+
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func encrypt(data []byte, passphrase string) []byte {
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
+
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
 	return ciphertext
 }
 
@@ -36,25 +40,42 @@ func decrypt(data []byte, passphrase string) []byte {
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	return plaintext
 }
 
 func encryptFile(filename string, data []byte, passphrase string) {
-	f, _ := os.Create(filename)
-	defer f.Close()
-	f.Write(encrypt(data, passphrase))
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer file.Close()
+	file.Write(encrypt(data, passphrase))
+}
+
+func encryptToDst(dst string, data []byte, key string) {
+	file, err := os.Create(dst)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer file.Close()
+
+	file.Write(encrypt(data, key))
 }
 
 func decryptFile(filename string, passphrase string) []byte {
@@ -63,17 +84,35 @@ func decryptFile(filename string, passphrase string) []byte {
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("You need to provide more arguments")
-		return
+	srcDir := os.Args[1]
+	dstDir := os.Args[2]
+	encKey := os.Args[3]
+	action := os.Args[4]
+
+	// Validate the input
+	if srcDir == "" {
+		log.Fatal("You need to provide a -src flag")
 	}
 
-	source, err := os.Open(os.Args[1])
-	if err != nil {
-		fmt.Println("Could not find source file")
-		return
+	if dstDir == "" {
+		log.Fatal("You need to provide a -dst flag")
 	}
 
-	destination := os.Args[2]
+	if encKey == "" {
+		log.Fatal("You need to provide an encryption key")
+	}
 
+	if !(action == "encrypt" || action == "decrypt") {
+		log.Fatal("You need to provide a ")
+	}
+
+	// Handle the actions
+	if action == "encrypt" {
+		srcData, err := ioutil.ReadFile(srcDir)
+		if err != nil {
+			log.Fatal("Could not read file", err)
+		}
+
+		encryptToDst(dstDir, srcData, encKey)
+	}
 }
