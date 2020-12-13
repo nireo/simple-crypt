c6e68ecf3e78aa58d2d653e16720fe0d16a349bc
nireo <eemil.lehtikangas1@gmail.com>
2020-12-13 17:19:04 +0200

    added some encryption functions

--- main.go
+++ main.go
@@ -1,52 +1,79 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
-	"github.com/mattn/go-gtk/glib"
-	"github.com/mattn/go-gtk/gtk"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+func encryptFile(filename string, data []byte, passphrase string) {
+	f, _ := os.Create(filename)
+	defer f.Close()
+	f.Write(encrypt(data, passphrase))
+}
+
+func decryptFile(filename string, passphrase string) []byte {
+	data, _ := ioutil.ReadFile(filename)
+	return decrypt(data, passphrase)
+}
+
 func main() {
-	gtk.Init(nil)
+	if len(os.Args) < 3 {
+		fmt.Println("You need to provide more arguments")
+		return
+	}
 
-	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
-	window.SetPosition(gtk.WIN_POS_CENTER)
-	window.SetTitle("GTK Go Input Dialog example!")
-	window.SetIconName("gtk-dialog-info")
-	window.Connect("destroy", func(ctx *glib.CallbackContext) {
-		println("got destroy!", ctx.Data().(string))
-		gtk.MainQuit()
-	}, "Happy coding!")
+	source, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Could not find source file")
+		return
+	}
 
-	dialog := gtk.NewDialog()
-	dialog.SetTitle("User input")
+	destination := os.Args[2]
 
-	vbox := dialog.GetVBox()
-
-	label := gtk.NewLabel("Enter some characters here :")
-	vbox.Add(label)
-
-	input := gtk.NewEntry()
-	input.SetEditable(true)
-	vbox.Add(input)
-
-	button := gtk.NewButtonWithLabel("OK")
-	button.Connect("clicked", func() {
-		fmt.Println("Input : ", input.GetText())
-		//gtk.MainQuit()
-	})
-
-	vbox.Add(button)
-
-	quitButton := gtk.NewButtonWithLabel("Quit")
-	quitButton.Connect("clicked", func() {
-		fmt.Println("Quiting now ...")
-		gtk.MainQuit()
-	})
-
-	vbox.Add(quitButton)
-
-	dialog.ShowAll()
-	window.Add(dialog)
-	gtk.Main()
 }
